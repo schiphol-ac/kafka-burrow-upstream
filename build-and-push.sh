fdbc3a53b17f51e3d61a66bdc4214457df32d9d9
Filidor Wiese <fili@fili.nl>
2020-09-09 11:40:42 +0200

    Updates to 1.3.4

--- build-and-push.sh
+++ build-and-push.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-VERSION="1.3.2"
+VERSION="1.3.4"
 IMAGE_TAG="schipholgroup/kafka-burrow:$VERSION"
+
+cd Burrow
+git checkout "v$VERSION"
 docker build -t $IMAGE_TAG Burrow/
 docker push $IMAGE_TAG
